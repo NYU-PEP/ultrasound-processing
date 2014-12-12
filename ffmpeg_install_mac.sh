38271b801abfe0befc1d66d24ecf28a2ddf56fbf
NYU Phonetics and Experimental Phonology Lab <nyu.phlab@gmail.com>
2014-12-12 16:38:48 -0500

    Update ffmpeg_install_mac.sh

--- ffmpeg_install_mac.sh
+++ ffmpeg_install_mac.sh
@@ -1,4 +1,4 @@
 curl -O http://ffmpegmac.net/resources/SnowLeopard_Lion_Mountain_Lion_Mavericks_Yosemite_08.12.2014.zip
 unzip -d ffmpeg SnowLeopard_Lion_Mountain_Lion_Mavericks_Yosemite_08.12.2014.zip
-cp -r ffmpeg /usr/local/bin/ffmpeg
 rm SnowLeopard_Lion_Mountain_Lion_Mavericks_Yosemite_08.12.2014.zip
+sudo cp -r ffmpeg /usr/local/bin/ffmpeg
