79486cf3c3622fb8e7810b95faa7ec668a542390
NYU Phonetics and Experimental Phonology Lab <nyu.phlab@gmail.com>
2014-12-18 15:27:02 -0500

    Update ffmpeg_install_mac.sh
    Might actually work now.

--- ffmpeg_install_mac.sh
+++ ffmpeg_install_mac.sh
@@ -1,4 +1,5 @@
 curl -O http://ffmpegmac.net/resources/SnowLeopard_Lion_Mountain_Lion_Mavericks_Yosemite_08.12.2014.zip
 unzip -d ffmpeg SnowLeopard_Lion_Mountain_Lion_Mavericks_Yosemite_08.12.2014.zip
 rm SnowLeopard_Lion_Mountain_Lion_Mavericks_Yosemite_08.12.2014.zip
-sudo cp -r ffmpeg /usr/local/bin/ffmpeg
+sudo cp -r ffmpeg/ /usr/bin/
+rm -r ffmpeg
